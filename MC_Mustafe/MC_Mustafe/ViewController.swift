cc36ae0012c11139ba29bf801d50cbc2dcc136dc
Mustafe Ahmed <160895798+Talaabo@users.noreply.github.com>
2024-05-31 08:59:01 -0400

    Initial mobile capstone

---
+++ MC_Mustafe/MC_Mustafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC_Mustafe
+//
+//  Created by Owner on 2024-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
